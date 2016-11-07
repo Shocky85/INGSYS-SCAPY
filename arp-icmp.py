314771f9d09f71243babbe2ba8b3b4465d465fbb
Guillaume Class <guillaume.class@gmail.com>
2016-11-07 18:22:25 +0100

    change

--- arp-icmp.py
+++ arp-icmp.py
@@ -4,8 +4,10 @@ import sys
 from scapy.all import *
  
 
+
 try:
-	alive,dead=sr( IP(dst="192.168.43.45")/TCP(dport=80,flags="S") )
+	alive,dead=sr(IP(dst="192.168.43.35-35")/ICMP())
+	print "MAC - IP"
 
 	alive.summary( lambda(s,r) : r.sprintf("%IP.src% is alive") )
 except:
